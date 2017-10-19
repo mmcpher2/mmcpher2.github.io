a70247d7ef42c6ee2846a45c6ff3541bda79f511
Ryan Mcpherson <ryanmcpherson@Ryans-MacBook-Pro.local>
2017-10-19 14:54:30 -0500

    Added pagination bar to Blog Page

--- JS/blog-controller.js
+++ JS/blog-controller.js
@@ -1,18 +1,18 @@
 
 
-// Parsing string from local storage back into objects
-const storedBlogs = JSON.parse(localStorage.getItem("blogArchiveString"))
 
 // Creating object to insert new HTML into current HTML
-let blogOut = document.getElementById("myBlogs")
+// let blogOut = document.getElementById("myBlogs")
 
-    for (let i = 0; i < storedBlogs.length; i++) {
-        let currentPost = storedBlogs[i]
-        blogOut.innerHTML +=
+//     for (let i = 0; i < storedBlogs.length; i++) {
+//         let currentPost = storedBlogs[i]
+//         blogOut.innerHTML +=
 
-            `<div class="blogPosts">
-                <h4>${currentPost.Title}</h4>            
-                <p>${currentPost.Week}</p>
-                <p>${currentPost.Post}</p>
-            </div>`
-    }
+//             `<div class="blogPosts">
+//                 <h4>${currentPost.Title}</h4>            
+//                 <p>${currentPost.Week}</p>
+//                 <p>${currentPost.Post}</p>
+//             </div>`
+//     }
+
+    
